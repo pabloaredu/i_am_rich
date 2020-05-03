e4104c88ca6501ce68bf5bdc7f82fa007fd3a271
Pablo <Pablo@Pablos-MBP.hitronhub.home>
2020-05-03 13:00:27 -0700

    Add app icon images

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ void main() {
         ),
         body: Center(
           child: Image(
-            image: AssetImage('images/diamond/png')
+            image: AssetImage('images/diamond.png'),
           ),
         ),
       ),
